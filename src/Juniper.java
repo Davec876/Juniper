6167575901c7634546970a3f5d2b8da9e1c2a840
Dave Chuck <dczhuo1388@gmail.com>
2021-02-15 18:10:14 -0500

    Initializing linkage between discord api and making bot live.

--- src/Juniper.java
+++ src/Juniper.java
@@ -1,8 +1,19 @@
+
+
+import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import javax.security.auth.login.LoginException;
 
 public class Juniper {
-    public static JDA jda;
-    public static void main(String[] args){
 
+    private static String discordToken = ("ODEwOTY2NDI1NzMxNzI3NDAw.YCrVVg.99-TBzS9N-3RpD4S0x9Z-3cF3Xk");
+
+    //referencing Bot linkage token.
+    public static void main(String[] args) throws LoginException{
+        JDABuilder builder = JDABuilder.createDefault(discordToken);
+
+        builder.build();
     }
+
 }
