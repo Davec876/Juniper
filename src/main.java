e3f033ddb6508ce3c9ff2be7ade10e4a262f8cc4
Justin Rackley <justinrackleyy@gmail.com>
2021-02-15 16:31:27 -0400

    original test

--- src/main.java
+++ src/main.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello");
+    }
 }
