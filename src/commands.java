9d2d09f0e076eea430ce91a33a7e60f6ab853e39
Dave Chuck <dczhuo1388@gmail.com>
2021-02-16 20:20:08 -0500

    Embedded Builder for all responses

--- src/commands.java
+++ src/commands.java
@@ -3,6 +3,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class commands extends ListenerAdapter {
+
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
         String[] args = event.getMessage().getContentRaw().split(" ");
 
@@ -11,7 +12,7 @@ public class commands extends ListenerAdapter {
 
             EmbedBuilder info = new EmbedBuilder();
             info.setTitle("Juniper");
-            info.setDescription("I'm a bot that Checks your mental health");
+            info.setDescription("I'm a bot that checks your mental health");
 //            info.addField("Creators","Dave, Justin, and Liam",false);
             info.setColor(20512750);
             //we can choose between the field or the footer whichever looks more professional.
@@ -24,24 +25,51 @@ public class commands extends ListenerAdapter {
         }
 
         if (args[0].equalsIgnoreCase(Juniper.prefix + "help")){
+
+            EmbedBuilder help = new EmbedBuilder();
+            help.setTitle("Juniper");
+            help.setDescription("Are you feeling:\nStressed\nAnxious\nDepressed\n");
+            help.setColor(20512750);
+
             event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessage("Are you feeling:\nStressed\nAnxious\nDepressed").queue();
+            event.getChannel().sendMessage(help.build()).queue();
 
         }
 
         if (args[0].equalsIgnoreCase(Juniper.prefix + "Stressed")){
+
+            EmbedBuilder Stressed = new EmbedBuilder();
+            Stressed.setTitle("Juniper");
+            Stressed.setDescription("You are Stressed");
+            Stressed.setColor(20512750);
+            Stressed.setFooter("You got this don't give up!");
+
             event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessage("You are stressed").queue();
+            event.getChannel().sendMessage(Stressed.build()).queue();
         }
 
         if (args[0].equalsIgnoreCase(Juniper.prefix + "Anxious")){
+
+            EmbedBuilder Anxious = new EmbedBuilder();
+            Anxious.setTitle("Juniper");
+            Anxious.setDescription("You are Anxious");
+            Anxious.setColor(20512750);
+            Anxious.setFooter("You got this don't give up!");
+
             event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessage("You are Anxious").queue();
+            event.getChannel().sendMessage(Anxious.build()).queue();
         }
 
         if (args[0].equalsIgnoreCase(Juniper.prefix + "Depressed")){
+
+            EmbedBuilder Depressed = new EmbedBuilder();
+            Depressed.setTitle("Juniper");
+            Depressed.setDescription("You are Depressed");
+            Depressed.setColor(20512750);
+            Depressed.setFooter("You got this don't give up!");
+
             event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessage("You are feeling Depressed").queue();
+            event.getChannel().sendMessage(Depressed.build()).queue();
         }
 
     }
